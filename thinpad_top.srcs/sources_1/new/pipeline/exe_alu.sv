961c5b2860de2f893a688e11aefca089f59ddc46
陈元昊 <chenyuan21@mails.tsinghua.edu.cn>
2023-11-25 00:22:55 +0800

    fix: try pcnt implement change

--- thinpad_top.srcs/sources_1/new/pipeline/exe_alu.sv
+++ thinpad_top.srcs/sources_1/new/pipeline/exe_alu.sv
@@ -30,7 +30,7 @@ module exe_alu #(
             // 10 -> ROTL
             4'b1010: y_i = (a_i << b_i[4:0]) | (a_i >> (DATA_WIDTH - b_i[4:0]));
             // 11 -> PCNT
-            4'b1011: y_i = $countones(a_i);
+            4'b1011: y_i = a_i[0] + a_i[1] + a_i[2] + a_i[3] + a_i[4] + a_i[5] + a_i[6] + a_i[7] + a_i[8] + a_i[9] + a_i[10] + a_i[11] + a_i[12] + a_i[13] + a_i[14] + a_i[15] + a_i[16] + a_i[17] + a_i[18] + a_i[19] + a_i[20] + a_i[21] + a_i[22] + a_i[23] + a_i[24] + a_i[25] + a_i[26] + a_i[27] + a_i[28] + a_i[29] + a_i[30] + a_i[31];
             // 12 -> PACK
             4'b1100: y_i = {b_i[DATA_WIDTH/2-1:0], a_i[DATA_WIDTH/2-1:0]};
             // 13 -> MINU
