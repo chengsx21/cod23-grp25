d8c9536be6cb3cb246077eacaba5d6a4d7cd0fdd
unknown <yf-xu21@mails.tsinghua.edu.cn>
2023-11-25 15:12:27 +0800

    perf:SRAM 状态合并

--- thinpad_top.srcs/sources_1/new/lab4/sram_controller.sv
+++ thinpad_top.srcs/sources_1/new/lab4/sram_controller.sv
@@ -31,94 +31,106 @@ module sram_controller #(
     output reg [SRAM_BYTES-1:0] sram_be_n
 );
 
-	// state machine
-	typedef enum logic [2:0] {
-		ST_IDLE,
-		ST_READ_1,
-		ST_READ_2,
-		ST_WRITE_1,
-		ST_WRITE_2,
-		ST_WRITE_3,
-		ST_DONE
-	} state_t;
-	state_t state;
+  // TODO: 实现 SRAM 控制器
 
-	wire [31:0] sram_data_i_comb;
-	reg [31:0] sram_data_o_comb;
-	reg sram_data_t_comb;
-	assign sram_data = sram_data_t_comb ? 32'bz : sram_data_o_comb;
-	assign sram_data_i_comb = sram_data;
+  // 状态列表
+  typedef enum logic [2:0] {
+    STATE_IDLE = 0,
+    STATE_READ = 1,
+    STATE_READ_2 = 2,
+    STATE_WRITE = 3,
+    STATE_WRITE_2 = 4,
+    STATE_WRITE_3 = 5,
+    STATE_DONE = 6
+  } state_t;
 
-  	always_ff @(posedge clk_i) begin
-		if (rst_i) begin
-			sram_ce_n <= 1'b1;
-			sram_oe_n <= 1'b1;
-			sram_we_n <= 1'b1;
-			wb_ack_o <= 1'b0;
-			state <= ST_IDLE;
-		end
-    	else begin
-			case (state)
-				ST_IDLE: begin
-					// wait for valid wishbone request
-					if (wb_cyc_i && wb_stb_i) begin
-						sram_ce_n <= 1'b0;
-						sram_we_n <= 1'b1;
-						sram_be_n <= ~wb_sel_i;
-						sram_addr <= (wb_adr_i >> 2);
-						if (wb_we_i) begin // write
-							sram_oe_n <= 1'b1;
-							sram_data_t_comb <= 1'b0;
-							sram_data_o_comb <= wb_dat_i;
-							state <= ST_WRITE_1;
-						end else begin // read
-							sram_oe_n <= 1'b0;
-							sram_data_t_comb <= 1'b1;
-							state <= ST_READ_1;
-						end
-					end
-				end
+  // 现态与次态
+  state_t state, n_state;  
 
-				ST_READ_1: begin
-					// wait for read data to be ready
-					state <= ST_READ_2;
-				end
+  reg  sram_data_t; // 0 for in, 1 for out(write).
+  wire [SRAM_DATA_WIDTH-1:0] sram_data_i;
+  reg  [SRAM_DATA_WIDTH-1:0] sram_data_o;
 
-				ST_READ_2: begin
-					// read data from sram and send to wishbone
-					wb_dat_o <= sram_data_i_comb;
-					sram_ce_n <= 1'b1;
-					sram_oe_n <= 1'b1;
-					wb_ack_o <= 1'b1;
-					state <= ST_DONE;
-				end
+  assign sram_data = sram_data_t? 32'bz: sram_data_o;
+  assign sram_data_i = sram_data;
 
-				ST_WRITE_1: begin
-					// wait for write data to be ready
-					sram_we_n <= 1'b0;
-					state <= ST_WRITE_2;
-				end
+  always_ff @(posedge clk_i) begin
+    if (rst_i) begin
+      state <= STATE_IDLE;
+    end
+    else begin
+      state <= n_state;
+    end
+  end
 
-				ST_WRITE_2: begin
-					// compute correct write data
-					sram_we_n <= 1'b1;
-					state <= ST_WRITE_3;
-				end
+  always_comb begin
+    case (state)
+      STATE_IDLE: begin
+        if (wb_stb_i & wb_cyc_i) begin
+          if (wb_we_i) begin
+            n_state = STATE_WRITE_2;
+          end
+          else begin
+            n_state = STATE_READ_2;
+          end
+        end
+        else begin
+          n_state = STATE_IDLE;
+        end
+      end 
+      STATE_WRITE_2: begin
+        n_state = STATE_WRITE_3;
+      end
+      STATE_WRITE_3: begin
+        n_state = STATE_IDLE;
+      end
+      STATE_READ_2: begin
+        n_state = STATE_IDLE;
+      end
+      default: begin
+        n_state = STATE_DONE;
+      end
+    endcase
+  end
 
-				ST_WRITE_3: begin
-					// write data to sram
-					sram_ce_n <= 1'b1;
-					wb_ack_o <= 1'b1;
-					state <= ST_DONE;
-				end
+  always_comb begin
 
-				ST_DONE: begin
-					// wait for wishbone to deassert
-					wb_ack_o <= 1'b0;
-					state <= ST_IDLE;
-				end
-			endcase
-    	end
-  	end
+    sram_addr = wb_adr_i >> 2'b10;
+    sram_data_t = ~ (wb_stb_i & wb_we_i);
+    sram_data_o = wb_dat_i;
+    
+    sram_ce_n = ~ wb_cyc_i;
+    sram_oe_n = ~ (wb_cyc_i & (~wb_we_i));
+    //sram_we_n = ~ (wb_cyc_i & wb_we_i);
+    sram_be_n = ~ wb_sel_i;
 
+    wb_ack_o = ((state == STATE_WRITE_3)|(state == STATE_READ_2));
+    wb_dat_o = sram_data;
+  end
+
+  always_ff @( posedge clk_i ) begin 
+    if (rst_i) begin
+      sram_we_n <= 1'b1;
+    end
+    else begin
+      case (n_state)
+        STATE_IDLE: begin
+
+        end
+        STATE_WRITE_2: begin
+          sram_we_n <= 1'b0;
+        end
+        STATE_WRITE_3: begin
+          sram_we_n <= 1'b1;
+        end
+        STATE_READ_2: begin
+
+        end
+        
+        default: begin
+
+        end
+      endcase
+    end
+  end
 endmodule
