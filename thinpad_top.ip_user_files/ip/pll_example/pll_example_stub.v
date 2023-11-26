c4b132fd19bb28b2a9c59293e7e9427494195a1b
陈元昊 <chenyuan21@mails.tsinghua.edu.cn>
2023-11-26 12:34:13 +0800

    fix: pll clk 25MHz

--- thinpad_top.ip_user_files/ip/pll_example/pll_example_stub.v
+++ thinpad_top.ip_user_files/ip/pll_example/pll_example_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-// Date        : Sun Nov 26 12:15:16 2023
+// Date        : Sun Nov 26 12:33:26 2023
 // Host        : DESKTOP-JLFA7QT running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               C:/Users/cyh/Desktop/rv-2023/cod23-grp25/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v
