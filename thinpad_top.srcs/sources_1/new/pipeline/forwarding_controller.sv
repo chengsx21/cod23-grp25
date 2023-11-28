f43382aa1288cbbe899f9871027c492aa13021b3
chengsx21 <chengsx_thu@163.com>
2023-11-28 13:12:00 +0800

    feat: init forwarding unit

---
+++ thinpad_top.srcs/sources_1/new/pipeline/forwarding_controller.sv
@@ -0,0 +1,11 @@
+module forwarding_controller #(
+    parameter DATA_WIDTH = 32,
+    parameter ADDR_WIDTH = 32
+) (
+    input wire [4:0] exe_rs1_i,
+    input wire [4:0] exe_rs2_i,
+    input wire [4:0] mem_rd_i,
+    input wire [4:0] mem_rd_i,
+    input wire [4:0] writeback_rd_i,
+    );
+endmodule
